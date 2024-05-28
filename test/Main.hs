56cc11b58a638813c8f3a153d765cbdf34f0dedf
Avimitin <dev@avimit.in>
2024-05-28 20:20:26 +0800

    Add unit test
    Signed-off-by: Avimitin <dev@avimit.in>

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,11 @@
 module Main (main) where
 
+import Md5 (MD5Context (MkMD5Context, regA), initMd5)
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+    describe "test-buffer-correctness" $ do
+        it "check A correct" $ do
+            let MkMD5Context{regA = a} = initMd5
+             in a `shouldBe` 1732584193
