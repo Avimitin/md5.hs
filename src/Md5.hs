0467c5284cc7b750235b40a489fa7a5251a7e410
Avimitin <dev@avimit.in>
2024-05-28 20:20:15 +0800

    Implement md5Init
    Signed-off-by: Avimitin <dev@avimit.in>

--- src/Md5.hs
+++ src/Md5.hs
@@ -1,4 +1,13 @@
-module Md5 where
+module Md5 (initMd5, MD5Context (..)) where
 
-myFunc :: IO ()
-myFunc = putStrLn "FUCK"
+data MD5Context = MkMD5Context
+    { -- Four buffer
+      regA :: Int
+    , regB :: Int
+    , regC :: Int
+    , regD :: Int
+    }
+
+-- initMd5 return a newly init MD5Context
+initMd5 :: MD5Context
+initMd5 = MkMD5Context{regA = 0x67452301, regB = 0xefcdab89, regC = 0x98badcfe, regD = 0x10325476}
